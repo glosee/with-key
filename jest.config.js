548e04c1f20f6bba7a02959bed678fb3733907d2
Graham <grahamlosee@gmail.com>
2020-08-31 16:42:46 -0400

    feat: add coverage report to test action
    update action to run and post the LCOV report in a comment on open Pull Requests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	setupFiles: ['<rootDir>/jest.setup.js'],
 	testPathIgnorePatterns: ['<rootDir>/node_modules/'],
 	transform: { '\\.(mjs|js)$': ['babel-jest'] },
-	coverageReporters: ['text'],
+	coverageReporters: ['text', 'lcov'],
 	collectCoverageFrom: [
 		'**/*.js',
 		'!**/node_modules/**',
